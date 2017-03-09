d01838df5acff57398cef0e2e1b6b27f3fc96c57
crisarevaloyr <c.arevalo@uniandes.edu.co>
2017-03-09 12:41:27 -0500

    corrección index

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // INTEGRACION DE LAS RUTAS EN LA APLICACION
 // ejecuta el codigo de JS cuando es llamado desde una URL por el cliente
-//app.use('/', index);
+app.use('/', index);
 //app.use('/hola', users);
 //app.use('/gatos', gatos);
 app.use('/cine', cine);
