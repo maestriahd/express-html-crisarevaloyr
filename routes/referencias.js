3d7531e1218bd7b4386a0a847218e8229755628a
crisarevaloyr <c.arevalo@uniandes.edu.co>
2017-03-09 12:38:20 -0500

    corrección de coma

--- routes/referencias.js
+++ routes/referencias.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.render('referencias',
     { title: 'Páginas que sirven de modelo para mi investigación',
-    texto: 'las referencias relacionadas en los links adjuntos, proponen 3 modelos distintos de distribución y circulación de productos cinematográficos'
+    texto: 'las referencias relacionadas en los links adjuntos, proponen 3 modelos distintos de distribución y circulación de productos cinematográficos',
     foto: 'http://i.giphy.com/wzy4qpGkG8PGE.gif'
   }
   );
